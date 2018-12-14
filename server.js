881829dc38a2aab2aaf157eb8aa3dc77cf012ecc
NGUYEN <baonguyen06091996@gmail.com>
2018-12-14 16:05:49 +0100

    button fermer sidebar fixé commentaires fixing

--- server.js
+++ server.js
@@ -69,12 +69,12 @@ app.get('/map', (req, res) => {
     console.log(req.query);
 
 //  res.send('coucou');
-    db.collection("fusion2").find({}).toArray(function(err, result) {
+    db.collection("fusionPlace").find().toArray(function(err, result) {
         if (err) return console.log(err);
         //console.log(result);
         if (result != null) {
             //console.log(typeof result[0].properties.vicinity);
-            res.json(result);
+            res.send(result);
         }
     });
 });
